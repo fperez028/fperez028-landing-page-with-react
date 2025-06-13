489f37177c4b9bbcee750b787c09e9c00b02f1cc
Frank Perez <21010185+fperez028@users.noreply.github.com>
2025-06-13 12:58:57 +0000

    adjustments to jumbotron

--- src/js/components/Jumbotron.jsx
+++ src/js/components/Jumbotron.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 export const Jumbotron = ({ title, description, buttonText }) => {
     return (
-        <header className="py-5">
-            <div className="container px-lg-5">
-                <div className="p-4 p-lg-5 bg-light rounded-3 text-center">
-                    <div className="m-4 m-lg-5">
-                        <h1 className="display-4 fw-bold">{title}</h1>
-                        <p className="fs-4">{description}</p>
+        <div className="py-5">
+            <div className="container">
+                <div className="p-4 bg-body-secondary rounded-3">
+                    <div className="m-4">
+                        <h1 className="display-2">{title}</h1>
+                        <p className="fs-5">{description}</p>
                         <a className="btn btn-primary btn-lg" href="#!">{buttonText}</a>
                     </div>
                 </div>
             </div>
-        </header>
+        </div>
     );
 };
