2f051f4cdd8b7a1def1b5c290033f2ec95bb1295
Frank Perez <21010185+fperez028@users.noreply.github.com>
2025-06-13 22:45:13 +0000

    added shadow to jumbotron

--- src/js/components/Jumbotron.jsx
+++ src/js/components/Jumbotron.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Jumbotron = ({ title, description, buttonText }) => {
     return (
         <div className="container py-5">
-            <div className="p-4 bg-body-secondary rounded-3">
+            <div className="p-4 shadow bg-body-secondary rounded-3">
                 <div className="m-4">
                     <h1 className="display-2">{title}</h1>
                     <p className="fs-5">{description}</p>
